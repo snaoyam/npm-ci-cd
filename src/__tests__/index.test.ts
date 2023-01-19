ecf1d3c23b43c70d5f0cb8ab38f119acdd7e4cc1
star <snaoyam@gmail.com>
2023-01-20 00:50:03 +0900

    init

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+import helloSparcs from '..';
+
+if(helloSparcs("sparcs") == undefined) {
+  throw new Error("helloSparcs should not return undefined")
+}
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export default function helloSparcs(name : String) : String {
+  return `Hello ${name}!`;
+}
